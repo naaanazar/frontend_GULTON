085c4f5f1571a8b86a098d53952ba1730194e152
Nazar Korchevsky <naaa.nazar@gmail.com>
2017-05-18 18:03:58 +0300

    add other pages

--- js/script.js
+++ js/script.js
@@ -1,23 +1,34 @@
 $(document).ready(function() {
 
-   $(".owl-carousel").owlCarousel({
-         loop: !0,
-         margin: 0,
-         responsiveClass: !0,
-         dotsEach: !0,
-         dotData: !0,
-         responsive: {
-            0: {
-               items: 1
-            },
-            600: {
-               items: 1
-            },
-            1e3: {
-               items: 1,
-               loop: !1
-            }
-         }
-      })
+//    $('.owl-carousel').owlCarousel({
+//    //  loop:true,
+//    //  margin:10,
+//     nav:true,
+//     dots: true,
+//     dotsEach: true,
+//     responsive:{
+//         0:{
+//             items:1
+//         },
+//         600:{
+//             items:1
+//         },
+//         1000:{
+//             items:1
+//         }
+//     }
+// })
+
+$("#lightSlider").lightSlider({
+   item: 1,
+   // auto: true,
+   loop: true,
+   mode: "slide",
+   useCSS: true,
+   // speed: 1000,
+   cssEasing: 'ease'
+});
+
+
 
 });

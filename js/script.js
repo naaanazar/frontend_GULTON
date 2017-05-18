4ec092367f6bf6287c529814aec1be5b61fa8243
Nazar Korchevsky <naaa.nazar@gmail.com>
2017-05-18 10:41:47 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+
+   $(".owl-carousel").owlCarousel({
+         loop: !0,
+         margin: 0,
+         responsiveClass: !0,
+         dotsEach: !0,
+         dotData: !0,
+         responsive: {
+            0: {
+               items: 1
+            },
+            600: {
+               items: 1
+            },
+            1e3: {
+               items: 1,
+               loop: !1
+            }
+         }
+      })
+
+});

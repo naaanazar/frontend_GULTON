748f7f71df179e2493e6aebcf9fbc7ef0ec05ab1
Nazar Korchevsky <naaa.nazar@gmail.com>
2017-05-25 12:15:14 +0300

    add typed js to home

--- js/animations.js
+++ js/animations.js
@@ -1,12 +1,57 @@
 var options = [
 
-    {selector: '.image-test', offset: 500, callback: function(el) {
-        Materialize.fadeInImage($(el));
-    } },
 
-    {selector: '.activate_bounceInDown', offset: 500, callback: function(el) {
-        $('.activate_bounceInDown').addClass('animated bounceInDown');
-        $('.activate_bounceInDown').css('opacity','1')
+
+    {selector: '.purp', offset: 500, callback: function(el) {
+        $(function(){
+            $(".typed_animated_part1").typed({
+                strings: ["Food"],
+                typeSpeed: 200,
+                callback: function() {
+                    $('.typed_animated_part1').next().remove()
+                    $(".typed_animated_part2").typed({
+                        strings: ["+"],
+                        typeSpeed: 200,
+                        callback: function() {
+                            $('.typed_animated_part2').next().remove()
+                            $(".typed_animated_part3").typed({
+                                strings: ["Us"],
+                                typeSpeed: 200,
+                                callback: function() {
+                                    $('.typed_animated_part3').next().remove()
+                                    $(".typed_animated_part4").typed({
+                                        strings: ["="],
+                                        typeSpeed: 200,
+                                        callback: function() {
+                                            $('.typed_animated_part4').next().remove()
+                                            $(".typed_animated_part5").typed({
+                                                strings: ["Soulmates"],
+                                                typeSpeed: 200,
+                                            });
+                                        }
+                                    });
+                                }
+                            });
+                        }
+                    });
+                }
+            });
+
+            /*$(".element2").typed({
+             strings: ["First sentence."],
+             typeSpeed: 100
+             });
+
+             $(".element3").typed({
+             strings: ["First sentence."],
+             typeSpeed: 100
+             });
+
+             $(".element4").typed({
+             strings: ["First sentence."],
+             typeSpeed: 100
+             });*/
+        });
     }},
 
     {selector: '.activate_zoomIn', offset: 500, callback: function(el) {

7a7d4414e3348219e242c744a84ae17f0cdb31ff
Dmytro <dima.efik.ua@gmail.com>
2017-05-25 10:00:13 +0300

    message

---
+++ js/page_active.js
@@ -0,0 +1,1 @@
+$(document).ready(function() {   var activPage =  $('body').data('page');   var activMenu = '.nav_menu_' + activPage;   $(activMenu).addClass('nav_menu_page_activ');});
---
+++ js/script.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+
+//    $('.owl-carousel').owlCarousel({
+//    //  loop:true,
+//    //  margin:10,
+//     nav:true,
+//     dots: true,
+//     dotsEach: true,
+//     responsive:{
+//         0:{
+//             items:1
+//         },
+//         600:{
+//             items:1
+//         },
+//         1000:{
+//             items:1
+//         }
+//     }
+// })
+
+$("#lightSlider").lightSlider({
+   item: 1,
+   // auto: true,
+   loop: true,
+   mode: "slide",
+   useCSS: true,
+   // speed: 1000,
+   cssEasing: 'ease'
+});
+
+
+
+});
